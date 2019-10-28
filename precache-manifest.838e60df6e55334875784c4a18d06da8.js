682953b5bb272a2e0931f9d4e667257bee1adf62
rogpe <prog_x@hotmail.com>
2019-10-28 11:17:51 -0300

    Updates

---
+++ precache-manifest.838e60df6e55334875784c4a18d06da8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab66797f1fc3eabef5d2cf2a1d1cb9f",
+    "url": "/reactReduxSimple/index.html"
+  },
+  {
+    "revision": "a7f192ca9a8bdd8fd5c5",
+    "url": "/reactReduxSimple/static/css/main.39fd657d.chunk.css"
+  },
+  {
+    "revision": "8c613d1618d3138eb774",
+    "url": "/reactReduxSimple/static/js/2.13040196.chunk.js"
+  },
+  {
+    "revision": "a7f192ca9a8bdd8fd5c5",
+    "url": "/reactReduxSimple/static/js/main.d6c4131d.chunk.js"
+  },
+  {
+    "revision": "3f52234b54441f0c5a0d",
+    "url": "/reactReduxSimple/static/js/runtime-main.b7815f82.js"
+  }
+]);
